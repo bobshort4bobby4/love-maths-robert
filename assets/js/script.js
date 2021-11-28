ab03b067302e7e09a20411704a40285c0f2bd44f
bobshort4bobby4 <treborken@gmail.com>
2021-11-28 12:27:35 +0000

    ADD html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You clicked ${gametype}`);
+            }
+        })
+
+    }
+})
+
+
+
+
 function runGame() {
 
 }
